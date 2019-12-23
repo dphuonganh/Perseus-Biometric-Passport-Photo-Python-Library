b752db2b75f92066414447c0e76d58b74d9f3611
dphuong <chuchimnhonhan@gmail.com>
2019-12-23 11:02:10 +0700

    Check file Valid

--- ham_chinh.py
+++ ham_chinh.py
@@ -1,33 +1,26 @@
-import argparse
-import cv2
-import face_recognition
+import os.path, face_recognition
+from os import path
+from PIL import Image
 
+def main():
+    """
+        @param: The file contains photos
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True,
-                help = "Path to the image")
-args = vars(ap.parse_args())
+        @return: Check the path file whether it exists or not
+    """
+    im = Image.open("./img/Brightness/camera.jpg")
 
-image = cv2.imread(args["image"])
+def check_file_exist(path):
+    path = input()
+    try:
+        with open(path, 'rb') as image_file:
+            image = face_recognition.load_image_file(image_file)
+            return image
+    except IOError:
+        print("File not valid")
 
-file = open("./img/Brightness/camera.jpg", "wb")
-print("Tên của file là: ", file.name)
-print("File đóng hoặc không? : ", file.closed)
-print("Chế độ mở file : ", file.mode)
-
-cv2.imshow("Image", image) # show img
-cv2.imwrite("./img/Brightness/camera.jpg", image) # save img
-cv2.waitKey(0)
-# RUNNN  python ham_chinh.py -i ./img/Brightness/camera.jpg
-
-
-# def check_file_exist():
-# try:
-#     with open(path, 'rb') as image_file:
-#         image = face_recognition.load_image_file(image_file)
-#         return image
-# except IOError:
-#     print("File not valid")
-
+if __name__== "__main__":
+   main()
+   check_file_exist(path)
 
 
