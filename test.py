b20388ebf77a1a117862b710e08a8b0109d12fa0
dphuong <chuchimnhonhan@gmail.com>
2019-12-27 17:27:22 +0700

    today

--- test.py
+++ test.py
@@ -1,109 +1,107 @@
-from PIL import Image, ImageFilter, ImageStat, ImageEnhance
-import os.path, face_recognition, argparse
-from os import path
+
+# from PIL import Image
+
+# img = Image.open('./img/Brightness/camera.jpg')
+# img = img.convert("RGBA")
+# datas = img.getdata()
+
+# newData = []
+# for item in datas:
+#     if item[0] == 255 and item[1] == 255 and item[2] == 255:
+#         newData.append((255, 255, 255, 0))
+#     else:
+#         if item[0] > 150:
+#             newData.append((0, 0, 0, 255))
+#         else:
+#             newData.append(item)
+#             print(item)
+
+# img.putdata(newData)
+# img.save("./img/Brightness/camera.jpg", "PNG")
 
 
-def main():
-    im = Image.open("./img/Brightness/camera.jpg")
+
+
+
+
+
+
+
+# def __init__(self, fd, background_color=None):
+#     try:
+#         if isinstance(fd, str):
+#             self.fd = Image.open(fd)
+#         elif isinstance(fd, Path):
+#             self.fd = Image.open(fd)
+#         elif isinstance(fd, BufferedReader):
+#             self.fd = Image.open(fd)
+#         elif isinstance(fd, PngImageFile):
+#             self.fd = fd
+#         if background_color == None:
+#             self.background_color = self.find_most_common_color(self.fd)
+#         else:
+#             self.background_color = background_color
+#     except FileNotFoundError:
+#         raise FileNotFoundError("FileNotFoundError")
+
+# @staticmethod
+
+
+# def detect_background_color(self, pixel_list):
+#         if self.fd.mode == "RGBA":
+#             background_color_list = []
+#             for i in range(len(pixel_list)):
+#                 for j in range(len(pixel_list[i])):
+#                     if pixel_list[i][j][3] == 0:
+#                         background_color_list.append(pixel_list[i][j])
+#             return background_color_list
+#         return find_most_common_color(self.fd)
+
+
+# def is_background_color(self, pixel, background_color):
+#     if isinstance(background_color, set):
+#         return pixel in background_color
+#     return np.all(pixel == background_color)
+
+
+
+# def detect_background_color(pixel_list, image):
+#     if image.mode == "RGBA":
+#         background_color_list = []
+#         for i in range(len(pixel_list)):
+#             for j in range(len(pixel_list[i])):
+#                 if pixel_list[i][j][3] == 0:
+#                     background_color_list.append(pixel_list[i][j])
+#         return background_color_list
+#     return find_most_common_color(image)
+
+
+# def is_background_color(pixel, background_color):
+#     if isinstance(background_color, set):
+#         return pixel in background_color
+#     return np.all(pixel == background_color)
+
+
+# if __name__ == "__main__":
+#     image = SpriteSheet("./img/Brightness/camera.jpg")
+#     new_image = image.create_sprite_labels_image()
+#     new_image.save("./img/Brightness/camera.jpg")
+
+
 
 def check_file_exist(path):
-    path = input()
     try:
         with open(path, 'rb') as image_file:
             image = face_recognition.load_image_file(image_file)
             return image
-    except IOError:
-        print("File not valid")
+    except OSError:
+        pass
 
 
-def enhance(self, factor):
-    return Image.blend(self.degenerate, self.image, factor)
-
-
-def color_detection(self, image):
-    self.image = image
-    self.intermediate_mode = 'L'
-    if 'A' in image.getbands():
-        self.intermediate_mode = 'LA'
-
-    self.degenerate = image.convert(self.intermediate_mode).convert(image.mode)
-
-
-def contrast_detection(self, image):
-        self.image = image
-        mean = int(ImageStat.Stat(image.convert("L")).mean[0] + 0.5)
-        self.degenerate = Image.new("L", image.size, mean).convert(image.mode)
-        if 'A' in image.getbands():
-            self.degenerate.putalpha(image.split()[-1])
-
-
-def brightness_detection(self, image):
-        self.image = image
-        self.degenerate = Image.new(image.mode, image.size, 0)
-        if 'A' in image.getbands():
-            self.degenerate.putalpha(image.split()[-1])
-        print(self.image)
-
-
-def sharpness_detection(self, image):
-        self.image = image
-        self.degenerate = image.filter(ImageFilter.SMOOTH)
-
-        if 'A' in image.getbands():
-            self.degenerate.putalpha(image.split()[-1])
-
-
-def result_color(image_file):
-    if color_detection(image_file) == True:
-        return True
-    else:
-        return False
-
-
-def result_brightness(image_file):
-    if brightness_detection(image_file) == True:
-        return True
-    else:
-        return False
-
-
-def result_sharpness(image_file):
-    if sharpness_detection(image_file) == True:
-        return True
-    else:
-        return False
-
-
-def result_contrast(image_file):      
-    if contrast_detection(image_file) == True:
-        return True
-    else:
-        return False
-
-parser = argparse.ArgumentParser(
-        description='Detects faces in the given image.')
-
-parser.add_argument(
-    "image_file", 
-    help='the image you\'d like to detect faces in.')
-
-parser.add_argument(
-    'mode', help='Set to call function need to do')
-
-args = parser.parse_args()
-
-
-if args.mode == 'color':
-    result_color(args.image_file)
-
-elif args.mode == 'brightness':
-    result_brightness(args.image_file)
-
-elif args.mode == 'sharpness':
-    result_sharpness(args.image_file)
-
-elif args.mode == "contrast":
-    result_contrast(args.image_file)
-
-
-# RUNN python test.py ./img/Brightness/camera.jpg Brightness
+def find_most_common_color(image):
+        color_count = defaultdict(int)
+        for pixel in image.getdata():
+            color_count[pixel] += 1
+        return max(color_count.items(), key=operator.itemgetter(1))[0]
+        print(find_most_common_color)
+find_most_common_color("./img/Brightness/camera.jpg")
